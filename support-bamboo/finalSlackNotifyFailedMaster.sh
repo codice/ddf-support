a26de344ab80cf86f861e2069bf94c9153ddb57e
Garrett Freibott <garrett.freibott@connexta.com>
2015-08-17 13:41:26 -0700

    Added Bamboo-Slack Notification Scripts
    Added two scripts for use on Bamboo's CI builds. Also updated the
    scripts' README file with notes on how to set the new scripts up on
    Bamboo.

---
+++ support-bamboo/finalSlackNotifyFailedMaster.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# On a failed master build, iterate through the plan's list of parents to find relevant commits and post a message to Slack with commits included in the message
+# Requires that 2 parameters be passed in: 1. Name of Slack channel to post to preceeded by a # sign, or it's ID. 2. The URL of the pre-configured Slack webhook.
+
+if [ "${bamboo_inject_jobResult}" != "Success" ]; then
+
+    echo "The build could not complete due to an error. Notifying Slack of build failure"
+    commitCount=`curl https://codice.atlassian.net/builds/rest/api/latest/result/${bamboo_planKey}-${bamboo_buildNumber}  | sed 's/.*changes size=\"//' | sed 's/\".*//'`
+
+    #If no changes, iterate to parent node
+    if [ ${commitCount} == "0" ]; then
+        # parses out the buildReason from the XML
+        lastParent="${bamboo_planKey}-${bamboo_buildNumber}"
+        parent=`curl https://codice.atlassian.net/builds/rest/api/latest/result/${lastParent} | sed 's/.*\<buildReason//' | sed 's/.*&quot;&gt;//' | sed 's/&lt;.*//'`
+
+        # loop until at the root node (note: the root node is one above the root plan)
+        while [[ ${parent: -1} == [0-9] ]]; do
+            lastParent=${parent}
+            parent=`curl https://codice.atlassian.net/builds/rest/api/latest/result/${lastParent} | sed 's/.*\<buildReason//' | sed 's/.*&quot;&gt;//' | sed 's/&lt;.*//'`
+        done
+        commitCount=`curl https://codice.atlassian.net/builds/rest/api/latest/result/${lastParent}  | sed 's/.*changes size=\"//' | sed 's/\".*//'`
+    fi
+
+    # If still no commits, blame manual run
+    if [ ${commitCount} == "0"  ]; then
+        curl -X POST --data-urlencode "payload={\"text\": \":warning: *Build Failure on Master* - <${bamboo_resultsUrl}|${bamboo_planName} - #${bamboo_buildNumber}>\n\`\`\`Manual Build by ${parent}\`\`\`\", \"channel\": \"$1\", \"username\": \"bamboo-bot\", \"icon_emoji\": \":bamboo:\"}" $2
+
+   # If commits, display offending commits
+    else
+        commitMessages=`git log --pretty=format:'%h %an %s' -n ${commitCount}`
+        curl -X POST --data-urlencode "payload={\"text\": \":warning: *Build Failure on Master* - <${bamboo_resultsUrl}|${bamboo_planName} - #${bamboo_buildNumber}> - Commits:\n\`\`\`${commitMessages}\`\`\`\", \"channel\": \"$1\", \"username\": \"bamboo-bot\", \"icon_emoji\": \":bamboo:\"}" $2
+
+    fi
+
+fi
